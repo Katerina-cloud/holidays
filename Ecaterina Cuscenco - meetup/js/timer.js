6f5a6d8c67fc23c6c7f07258d6d60b8b9df1cce0
Kate <ekaterina.kuschenko@gmail.com>
2020-01-03 22:19:24 +0300

    empty files deleted

--- Ecaterina Cuscenco - meetup/js/timer.js
+++ Ecaterina Cuscenco - meetup/js/timer.js
@@ -1,26 +1,25 @@
 const second = 1000,
-minute = second * 60,
-hour = minute * 60,
-day = hour * 24;
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
 
 let countDown = new Date('Jan 30, 2020 00:00:00').getTime(),
-x = setInterval(function() {
+  x = setInterval(function() {
 
   let now = new Date().getTime(),
-  distance = countDown - now;
+    distance = countDown - now;
 
-  let dayStr = Math.floor(distance / (day)).toString()
-  let hourStr = Math.floor((distance % (day)) / (hour)).toString()
-  let minuteStr = Math.floor((distance % (hour)) / (minute)).toString()
-  let secondStr = Math.floor((distance % (minute)) / second).toString()
+  let dayStr = Math.floor(distance / (day)).toString(),
+    hourStr = Math.floor((distance % (day)) / (hour)).toString(),
+    minuteStr = Math.floor((distance % (hour)) / (minute)).toString(),
+    secondStr = Math.floor((distance % (minute)) / second).toString();
 
-  dayStr = dayStr.length === 1 ? '0' + dayStr : dayStr;
-  hourStr = hourStr.length === 1 ? '0' + hourStr : hourStr;
-  minuteStr = minuteStr.length === 1 ? '0' + minuteStr : minuteStr;
-  secondStr = secondStr.length === 1 ? '0' + secondStr : secondStr;
+  document.getElementById('days').innerText = setDoubleDigit(dayStr);
+  document.getElementById('hours').innerText = setDoubleDigit(hourStr);
+  document.getElementById('minutes').innerText = setDoubleDigit(minuteStr);
+  document.getElementById('seconds').innerText = setDoubleDigit(secondStr);
+}, second);
 
-  document.getElementById('days').innerText = dayStr,
-  document.getElementById('hours').innerText = hourStr,
-  document.getElementById('minutes').innerText = minuteStr,
-  document.getElementById('seconds').innerText = secondStr;
-}, second)
+function setDoubleDigit(str) {
+  return str.length === 1 ? '0' + str : str;
+}
