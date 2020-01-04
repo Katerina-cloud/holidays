5ee436f7b85b98ef4cb9c3ef6e761cf838431fe6
Kate <ekaterina.kuschenko@gmail.com>
2020-01-04 10:43:12 +0300

    task 3 added

---
+++ task3.js
@@ -0,0 +1,112 @@
+// task 1
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+function findOutlier(integers){
+  let odd = integers.filter(num => num % 2 !== 0);
+  let even = integers.filter(num => num % 2 === 0);
+
+  return odd.length === 1 ? odd[0] : even[0];  
+}
+
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 2, 3]));
+console.log(findOutlier([2,6,8,10,3]));
+console.log(findOutlier([0,0,3,0,0]));
+console.log(findOutlier([1,1,0,1,1]));
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+// task 2
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// Examples input/output:
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+  let strToArray = Array.from(str.toLowerCase());
+  let countX = 0;
+  let countY = 0;
+  strToArray.forEach( item => {
+    if(item === "x") {
+      countX++;
+    } 
+    else if (item === "o") {
+      countY++;
+    }
+  })
+  return countX === countY;
+}
+
+console.log(XO('xo'));
+console.log(XO("xxOo"));
+console.log(XO("xxxm"));
+console.log(XO("Oo"));
+console.log(XO("ooom"));
+
+// task 3
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(str){
+  const shortestWordArray = str.split(' ');
+  let shortestWordLength = shortestWordArray[0].length;
+  shortestWordArray.forEach(item => {
+    if(item.length < shortestWordLength) {
+      shortestWordLength = item.length;
+    }
+  })
+  return shortestWordLength;
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+console.log(findShort("turns out random test cases are easier than writing out basic ones"));
+
+// task 4
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, and u as vowels for this Kata.
+// The input string will only consist of lower case letters and/or spaces.
+function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
+
+console.log(getCount("abracadabra"));
+console.log(getCount('o a kak ushakov lil vo kashu kakao'));
+console.log(getCount('my pyx'));
+
+// task 5
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+  return l.filter( item => {return typeof(item) === 'number'});
+}
+
+console.log(filter_list([1,2,'a','b']))
+console.log(filter_list([1,'a','b',0,15]))
+console.log(filter_list([1,2,'aasf','1','123',123]))
+
+// task 7
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map(item => {
+    return item[0].toUpperCase() + item.slice(1)
+  }).join(' ');
+};
+
+console.log("How can mirrors be real if our eyes aren't real".toJadenCase());
